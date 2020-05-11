c78656a408e00c2469fbf49f8f774cf13848b417
Gulam Qasim <gfram@hotmail.co.uk>
2020-05-11 11:44:41 +0100

    Updated Hex Colours

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ const button = document.querySelector('button');
 const body = document.querySelector('body');
 
 // We assign our colours within an array
-const colors = ['red', 'green', 'blue', 'pink', 'purple'];
+const colors = ['#ff6699', '#99cccc', '#000066', '#333366', '#6699cc',
+  '#003333', '#c05030', '#cdb5cd', '#339999', '#000000'];
 
 // Adds a click event to the button
 button.addEventListener('click', changeBackground);
 
 function changeBackground() {
+  // Makes the colours go in a random order
   const colorIndex = parseInt(Math.random()*colors.length);
-  body.style.backgroundColor = colors[colorIndex]
+  // Applies our background colours
+  body.style.backgroundColor = colors[colorIndex];
 }
