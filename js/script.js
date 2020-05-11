762e97d07b69a6e4e27f661b065767b0c188ed44
Gulam Qasim <gfram@hotmail.co.uk>
2020-05-11 11:36:44 +0100

    Added Background Colours

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-//Chooses a random colour
+//This two lines are locating 'button' & 'body' and can minipulate them
 const button = document.querySelector('button');
+const body = document.querySelector('body');
+
+// We assign our colours within an array
+const colors = ['red', 'green', 'blue', 'pink', 'purple'];
+
+// Can assign the colour
+body.style.changeBackground = 'violet';
+
+// Adds a click event to the button
+button.addEventListener('click', changeBackground)
+
+function changeBackground() {
+const colorIndex = parseInt(Math.random()*colors.length)
+body.style.backgroundColor = colors[colorIndex]
+}
